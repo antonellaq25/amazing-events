748001c5f5042ef9e3bcf2f234b021c681076c60
Antonella Quintana <antonellaq25@gmail.com>
2023-02-24 22:14:42 -0300

    add description

---
+++ html/details.js
@@ -0,0 +1,32 @@
+fetch('../data.json')
+    .then((response) => response.json())
+    .then((json) => { 
+        const params = new URLSearchParams(window.location.search);
+       let id =  params.get('id');
+        const result = json.events.filter(event=> event.id == id);
+        for (i = 0; i < result.length; i++) {
+            console.log(result[i].name);
+			addEvents(result[i]);
+        }
+      }
+    );
+function addEvents(event){
+   const cardSection = document.querySelector("#details-row");
+	 cardSection.innerHTML += 
+	 `  <div class="col-md-6">
+     <img src=${event.image} class="p-2 w-100 img-fluid " alt="...">
+ </div>
+ <div class="col-md-6">
+     <div class="card-body">
+         <h5 class="card-title">${event.name}</h5>
+         <p class="card-text">Date:${event.date}</p>
+         <p class="card-text">Description:${event.description}</p>
+         <p class="card-text">Category:${event.category}</p>
+         <p class="card-text">Place:${event.place}</p>
+         <p class="card-text">Capacity:${event.capacity}</p>
+         <p class="card-text">Assistance:${event.assistance}</p>
+         <p class="card-text">Price:${event.price}</p>
+     </div>
+ </div>`	
+}
+
---
+++ html/past-events.js
@@ -0,0 +1,35 @@
+fetch('../data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        const result = json.events.filter(event=> checkDate(event.date, json.currentDate));
+        for (i = 0; i < result.length; i++) {
+            console.log(result[i].name);
+			addEvents(result[i]);
+        }
+      }
+    );
+function addEvents(event){
+   const cardSection = document.querySelector("#past-events-row");
+	 cardSection.innerHTML += 
+	 ` <div class="col-sm-3 mb-3 mb-sm-0">
+	 <div class="card text-center">
+			 <img src=${event.image} class="card-img-top " alt="...">
+			 <div class="card-body">
+					 <h5 class="card-title">${event.name}</h5>
+					 <p class="card-text">${event.description}</p>
+					 <div class="d-flex justify-content-between ">
+							 <div>
+									 Price ${event.price}
+							 </div>
+                             <a href="details.html?id=${event.id}" class="btn btn-primary">Ver mas...</a>
+					 </div>
+			 </div>
+	 </div>
+</div>`	
+}
+
+function checkDate(eventDate, currentDate){
+    var g1 = new Date(currentDate);
+    var g2 = new Date(eventDate);
+    return g2 < g1;
+}
